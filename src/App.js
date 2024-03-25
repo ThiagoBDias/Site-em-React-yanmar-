f958fb11b7bdfddbbdf88281261a5ba296781cf6
Thiago Batista <Thiagobatistadiass@gmail.com>
2024-03-25 09:40:18 -0300

    web site

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-import './App.css';
 import PaginaInicial from './components/PaginaInicial';
 
 function App() {
--- src/components/PaginaInicial.js
+++ src/components/PaginaInicial.js
@@ -1,10 +1,28 @@
+
+import  logo from "./Img/capa.jpg"
+
 function PaginaInicial(){
+  const divStyle = {
+    backgroundImage: `url(${logo})`,
+    backgroundSize: 'cover',
+    backgroundRepeat: 'no-repeat',
+    backgroundPosition: 'center',
+    textAlign: 'center',
+    minHeight: '100vh', // Define a altura da div para ocupar toda a altura da página
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+    flexDirection: 'column'
+  };
+  
 
   return ( 
-    <div>
-      Meu primeiro component
+    <div style={divStyle}>
+    <div style={{ backgroundColor: 'rgba(255, 255, 255, 0.7)', padding: '20px', borderRadius: '5px', textAlign: 'center' }}>
+      <h2>teste</h2>
+      <button>Conheça nossos modelos </button>
     </div>
-
+  </div>
   )
 
 }
